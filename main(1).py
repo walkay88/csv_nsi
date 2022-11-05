17a016f997baaf16bae40bf92f6b257767f9e5b2
Walkay <67157967+walkay88@users.noreply.github.com>
2022-11-05 15:27:16 +0100

    g'vrtvrvr

--- main(1).py
+++ main(1).py
@@ -1,6 +1,13 @@
-def cherche_couleur ():
-    liste=[]
-    liste_charlie=[]
-    for element in liste ():
-        if element == 'bleu':
-            
+def couleur(nom_fichier):
+ 
+    fichier=open(nom_fichier,'r')   
+    liste=0                          
+    for elt in fichier:  
+        if elt =='bleu':
+              liste = liste +1
+
+    fichier.close()                 
+    return liste                      
+ 
+f='ou_est_charlieV2_ANSI.csv'       
+liste=couleur(f)           
